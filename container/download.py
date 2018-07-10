ecb8ba7d32eba989462272eb24d44cef23fbcdb5
Yang Cheng <b05705014@ntu.edu.tw>
2018-07-10 14:37:20 +0800

    Update the coding style in `download.py`

--- container/download.py
+++ container/download.py
@@ -2,17 +2,21 @@ import urllib.request
 import sys
 
 
-def get_one_file(url,file_name,size_KB):
-    size_KB=int(size_KB)
-    req=urllib.request.urlopen(url)
-    filename=file_name
-    _file=open(filename,'wb')
-    _file.write(req.read())
-    get_bytes=_file.seek(0,2)
-    if get_bytes>=size_KB*1000:
+def get_one_file(url, file_name, size_KB):
+    size_KB = int(size_KB)
+    req = urllib.request.urlopen(url)
+    filename = file_name
+    file = open(filename, 'wb')
+    file.write(req.read())
+    get_bytes = file.seek(0, 2)
+    if get_bytes >= size_KB * 1000:
         print('Download finished')
     else:
-        print("Download unfinished,retry...")
-        get_one_file(url,file_name,size_KB)
+        print('Download unfinished,retrying...')
+        get_one_file(url, file_name, size_KB)
 
-get_one_file(sys.argv[1],sys.argv[2],sys.argv[3])
+def main():
+    get_one_file(sys.argv[1], sys.argv[2], sys.argv[3])
+
+if __name__ == '__main__':
+    main()
