193c6aad2ff673731b41a3cadb5644f14cf82d03
Yang Cheng <b05705014@ntu.edu.tw>
2018-07-27 15:19:33 +0800

    Modify `compare.py`

--- container/build/compare.py
+++ container/build/compare.py
@@ -4,6 +4,9 @@ import subprocess
 from io import StringIO
 
 count=0
+
+
+
 def check_benchmark(dir_to_check):
     global count
     all_dir=os.listdir(dir_to_check)
@@ -13,14 +16,34 @@ def check_benchmark(dir_to_check):
             check_benchmark(next_dir)
         else:
             if 'smt2' in j:
+                flag1=0
+                flag2=0
                 count += 1
                 try:
-                    output = str(subprocess.check_output('./Trau '+next_dir, shell=True))
-                    if output[-6:-3] == "SAT":
-                        print(count,next_dir,': SAT')
+                    #os.system("python2 run.py -f "+j)
+                    output1 = str(subprocess.check_output('./Trau '+next_dir, shell=True))
+                    if 'UNSAT' in output1:
+                        flag1=1
+                        print(count,next_dir,': NOT by trau')
                     else:
-                        print(count,next_dir,': NOT')
+                        print(count,next_dir,': SAT by trau')
                 except:
-                    print(count,next_dir,': ERROR')
+                    flag1 = 2
+                    print(count,next_dir,': ERROR by trau')
+
+                try:
+                    output2 = str(subprocess.check_output('python2 run.py -f '+next_dir))
+                    if 'UNSAT' in output2:
+                        flag2=1
+                        print(count,next_dir,': NOT by s3p')
+                    else:
+                        print(count,next_dir,': SAT by s3p')
+                except:
+                    print(count,next_dir,': ERROR by s3p')
+                    flag2 = 2
+                if flag1 == flag2:
+                    print(count,next_dir,"SAME")
+                else:
+                    print(count,next_dir,"DIFF")
 
 check_benchmark(input("input test data location:"))
