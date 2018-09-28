8ad2432487858723eefa640bf87d90123229c7ca
Hung-Wei Hsu <hsuhw@livemail.tw>
2018-09-28 14:54:16 +0800

    Add scripts `linearize` and `quadrify`

---
+++ src/main/scripts/quadrify.py
@@ -0,0 +1,19 @@
+import sys
+
+import parsing.basic
+import codegen.smtlib
+import solver
+
+sys.setrecursionlimit(4000)
+
+
+def main(argv):
+    file_name = argv[1].split('/')[-1]
+    problem = parsing.basic.parse_file(argv[1])
+    problem.merge_all_word_equations()
+    solver.turn_to_quadratic_we(problem)
+    codegen.smtlib.to_file(problem, f'benchmarks/quadratic/{file_name}')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
